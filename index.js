cd455dcd1ee02e314b979fd8badce093e3678d60
Ronnie <ron.howell0326@gmail.com>
2022-10-17 21:07:16 -0400

    commit tree 32aeaad5f3ae3a291ff4a8c0a3f2d030585c8744

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let mainElement = document.querySelector('#main').remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.setAttribute = 'h1#victory';
+newHeader.innerHTML = 'Ron is the champion';
